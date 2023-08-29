287ce5026a4f0f947551b790f73d445a483ae932
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-29 10:21:27 +0330

    Add files via upload

---
+++ ConsoleApp113/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            for (count = 1; count <= 10; ++count)
+            {
+                if (count == 5)
+                {
+                    break;
+                }
+                Console.Write($"{count}");
+            }
+            Console.WriteLine($"\nBroke out of loop at count ={count}");
+            Console.ReadLine();
+        }
+    }
+}
